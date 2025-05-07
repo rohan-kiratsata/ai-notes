2fcac460fb911601403a0dee49c906f54a8f1bc8
rohan-kiratsata <kiratsatarohan1234@gmail.com>
2025-05-08 02:10:47 +0530

    INP: session mgmt bug

--- utils/supabase/middleware.ts
+++ utils/supabase/middleware.ts
@@ -2,10 +2,8 @@ import { createServerClient } from "@supabase/ssr";
 import { NextResponse, type NextRequest } from "next/server";
 
 export async function updateSession(request: NextRequest) {
-  let response = NextResponse.next({
-    request: {
-      headers: request.headers,
-    },
+  let supabaseResponse = NextResponse.next({
+    request,
   });
 
   const supabase = createServerClient(
@@ -13,63 +11,60 @@ export async function updateSession(request: NextRequest) {
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
     {
       cookies: {
-        get(name: string) {
-          return request.cookies.get(name)?.value;
+        // @ts-ignore
+        getAll() {
+          return request.cookies.getAll();
         },
-        set(
-          name: string,
-          value: string,
-          options: {
-            path: string;
-            maxAge?: number;
-            domain?: string;
-            secure?: boolean;
-            httpOnly?: boolean;
-            sameSite?: "strict" | "lax" | "none";
-          }
-        ) {
-          request.cookies.set({
-            name,
-            value,
-            ...options,
+        // @ts-ignore
+        setAll(cookiesToSet) {
+          cookiesToSet.forEach(({ name, value, options }: any) =>
+            request.cookies.set(name, value),
+          );
+          supabaseResponse = NextResponse.next({
+            request,
           });
-          response = NextResponse.next({
-            request: {
-              headers: request.headers,
-            },
-          });
-          response.cookies.set({
-            name,
-            value,
-            ...options,
-          });
-        },
-        remove(
-          name: string,
-          options: {
-            path: string;
-            domain?: string;
-            secure?: boolean;
-            httpOnly?: boolean;
-            sameSite?: "strict" | "lax" | "none";
-          }
-        ) {
-          request.cookies.delete(name);
-          response = NextResponse.next({
-            request: {
-              headers: request.headers,
-            },
-          });
-          response.cookies.delete({
-            name,
-            ...options,
-          });
+          cookiesToSet.forEach(({ name, value, options }: any) =>
+            supabaseResponse.cookies.set(name, value, options),
+          );
         },
       },
-    }
+    },
   );
 
-  await supabase.auth.getUser();
+  // Do not run code between createServerClient and
+  // supabase.auth.getUser(). A simple mistake could make it very hard to debug
+  // issues with users being randomly logged out.
 
-  return response;
+  // IMPORTANT: DO NOT REMOVE auth.getUser()
+
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+  console.log("user:", user);
+
+  if (
+    !user &&
+    !request.nextUrl.pathname.startsWith("/login") &&
+    !request.nextUrl.pathname.startsWith("/auth")
+  ) {
+    // no user, potentially respond by redirecting the user to the login page
+    const url = request.nextUrl.clone();
+    url.pathname = "/login";
+    return NextResponse.redirect(url);
+  }
+
+  // IMPORTANT: You *must* return the supabaseResponse object as it is.
+  // If you're creating a new response object with NextResponse.next() make sure to:
+  // 1. Pass the request in it, like so:
+  //    const myNewResponse = NextResponse.next({ request })
+  // 2. Copy over the cookies, like so:
+  //    myNewResponse.cookies.setAll(supabaseResponse.cookies.getAll())
+  // 3. Change the myNewResponse object to fit your needs, but avoid changing
+  //    the cookies!
+  // 4. Finally:
+  //    return myNewResponse
+  // If this is not done, you may be causing the browser and server to go out
+  // of sync and terminate the user's session prematurely!
+
+  return supabaseResponse;
 }
--- utils/supabase/server.ts
+++ utils/supabase/server.ts
@@ -2,23 +2,30 @@ import { createServerClient } from "@supabase/ssr";
 import { cookies } from "next/headers";
 
 export async function createClient() {
-  const cookieStore = cookies();
+  const cookieStore = await cookies();
 
   return createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
     {
       cookies: {
-        async get(name) {
-          return (await cookieStore).get(name)?.value;
+        // @ts-ignore
+        getAll() {
+          return cookieStore.getAll();
         },
-        async set(name, value, options) {
-          (await cookieStore).set({ name, value, ...options });
-        },
-        async remove(name, options) {
-          (await cookieStore).set({ name, value: "", ...options });
+        // @ts-ignore
+        setAll(cookiesToSet) {
+          try {
+            cookiesToSet.forEach(({ name, value, options }: any) =>
+              cookieStore.set(name, value, options),
+            );
+          } catch {
+            // The `setAll` method was called from a Server Component.
+            // This can be ignored if you have middleware refreshing
+            // user sessions.
+          }
         },
       },
-    }
+    },
   );
 }
